b1d0778b016e8db897ecffb57dc0de39451f737a
Abdurrahman <emailkamu@example.com>
2026-01-31 14:00:07 +0700

    main.tsx update

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
+// import { createRoot } from "react-dom/client";
+// import App from "./App.tsx";
+// import "./index.css";
+
+// createRoot(document.getElementById("root")!).render(<App />);
+
+// main.tsx
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Failed to find the root element");
+
+// Pastikan import "./firebase" dilakukan di sini jika App.tsx belum melakukannya
+import "./lib/firebase"; 
+
+createRoot(rootElement).render(<App />);
