0a126a47a5a2a264af25028edd7c0f457caee593
Abdurrahman <emailkamu@example.com>
2026-02-28 22:31:43 +0700

    inject analitycs vercel

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import { inject } from '@vercel/analytics'; // Tambahkan ini
+
+inject(); // Panggil fungsi ini
+
 createRoot(document.getElementById("root")!).render(<App />);
